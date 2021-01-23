7cd52cedd203f0c0e6736861d09280e1212355fc
unknown <mailmehois@gmail.com>
2021-01-22 23:39:23 -0500

    fix: improved code formatting

--- newsline.py
+++ newsline.py
@@ -1,45 +1,72 @@
-from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-from colorama import init, Fore, Style
-from pyshorteners import Shortener
-import requests
 
+import requests
+from bs4 import BeautifulSoup
+from colorama import Fore, Style, init
+from pyshorteners import Shortener
 
 init(autoreset=True)  # Initilize colorama
 
-top = 'https://news.google.com/topstories?hl=en-US&gl=US&ceid=US:en'
-us = 'https://news.google.com/topics/CAAqIggKIhxDQkFTRHdvSkwyMHZNRGxqTjNjd0VnSmxiaWdBUAE?hl=en-US&gl=US&ceid=US%3Aen'
-world = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=' \
-        'US&ceid=US%3Aen'
-business = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=' \
-           'US&ceid=US%3Aen'
-technology = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=' \
-             'US&ceid=US%3Aen'
-entertainment = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=' \
-                'US&ceid=US%3Aen'
-sports = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp1ZEdvU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=' \
-         'US&ceid=US%3Aen'
-science = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp0Y1RjU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=' \
-          'US&ceid=US%3Aen'
-health = 'https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNR3QwTlRFU0FtVnVLQUFQAQ?hl=en-US&gl=US&ceid=US%3Aen'
+top = "https://news.google.com/topstories?hl=en-US&gl=US&ceid=US:en"
+us = (
+    "https://news.google.com/topics"
+    "/CAAqIggKIhxDQkFTRHdvSkwyMHZNRGxqTjNjd0VnSmxiaWdBUAE?hl=en-US&gl=US"
+    "&ceid=US%3Aen"
+)
+world = (
+    "https://news.google.com/topics"
+    "/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US"
+    "&gl=US&ceid=US%3Aen"
+)
+business = (
+    "https://news.google.com/topics"
+    "/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB?hl=en"
+    "-US&gl=US&ceid=US%3Aen"
+)
+technology = (
+    "https://news.google.com/topics"
+    "/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0FtVnVHZ0pWVXlnQVAB?hl"
+    "=en-US&gl=US&ceid=US%3Aen"
+)
+entertainment = (
+    "https://news.google.com/topics"
+    "/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtVnVHZ0pWVXlnQVAB"
+    "?hl=en-US&gl=US&ceid=US%3Aen"
+)
+sports = (
+    "https://news.google.com/topics"
+    "/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp1ZEdvU0FtVnVHZ0pWVXlnQVAB?hl=en-US"
+    "&gl=US&ceid=US%3Aen"
+)
+science = (
+    "https://news.google.com/topics"
+    "/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp0Y1RjU0FtVnVHZ0pWVXlnQVAB?hl=en"
+    "-US&gl=US&ceid=US%3Aen"
+)
+health = (
+    "https://news.google.com/topics"
+    "/CAAqIQgKIhtDQkFTRGdvSUwyMHZNR3QwTlRFU0FtVnVLQUFQAQ?hl=en-US&gl=US"
+    "&ceid=US%3Aen"
+)
 
 news_limit = 8
 
 
 def get_news(url):
-
     source = requests.get(url).text
-    soup = BeautifulSoup(source, 'lxml')
-    articles = soup.find_all('a', class_='DY5T1d RZIKme', limit=news_limit)
+    soup = BeautifulSoup(source, "lxml")
+    articles = soup.find_all("a", class_="DY5T1d RZIKme", limit=news_limit)
 
     for article in articles:
-        short_url = Shortener().tinyurl.short(urljoin(url, article.get('href')))
+        short_url = Shortener().tinyurl.short(urljoin(url, article.get("href")))
         print(Fore.CYAN + Style.BRIGHT + article.string)
-        print(Style.DIM + short_url + '\n')
+        print(Style.DIM + short_url + "\n")
 
 
-print(Fore.CYAN + 'Options:')
-print(Fore.GREEN + """t - Top stories
+print(Fore.CYAN + "Options:")
+print(
+    Fore.GREEN
+    + """t - Top stories
 us - U.S. news
 w - World news (excluding U.S.)
 b - Business news
@@ -49,41 +76,42 @@ s - Sports news
 sc - Science news
 h - Health news
 q - Quit
-""")
+"""
+)
 
 while True:
 
-    option = input('Choose an option: ').lower()
+    option = input("Choose an option: ").lower()
 
-    if option == 't':
-        print(Fore.GREEN + 'Top stories:' + '\n')
+    if option == "t":
+        print(Fore.GREEN + "Top stories:" + "\n")
         get_news(top)
-    elif option == 'us':
-        print(Fore.GREEN + 'U.S. news' + '\n')
+    elif option == "us":
+        print(Fore.GREEN + "U.S. news" + "\n")
         get_news(us)
-    elif option == 'w':
-        print(Fore.GREEN + 'World news:' + '\n')
+    elif option == "w":
+        print(Fore.GREEN + "World news:" + "\n")
         get_news(world)
-    elif option == 'b':
-        print(Fore.GREEN + 'Business news:' + '\n')
+    elif option == "b":
+        print(Fore.GREEN + "Business news:" + "\n")
         get_news(business)
-    elif option == 'te':
-        print(Fore.GREEN + 'Technology news:' + '\n')
+    elif option == "te":
+        print(Fore.GREEN + "Technology news:" + "\n")
         get_news(technology)
-    elif option == 'e':
-        print(Fore.GREEN + 'Entertainment news:' + '\n')
+    elif option == "e":
+        print(Fore.GREEN + "Entertainment news:" + "\n")
         get_news(entertainment)
-    elif option == 's':
-        print(Fore.GREEN + 'Sports news:' + '\n')
+    elif option == "s":
+        print(Fore.GREEN + "Sports news:" + "\n")
         get_news(sports)
-    elif option == 'sc':
-        print(Fore.GREEN + 'Science news:' + '\n')
+    elif option == "sc":
+        print(Fore.GREEN + "Science news:" + "\n")
         get_news(science)
-    elif option == 'h':
-        print(Fore.GREEN + 'Health news:' + '\n')
+    elif option == "h":
+        print(Fore.GREEN + "Health news:" + "\n")
         get_news(health)
-    elif option == 'q':
-        print(Fore.RED + 'Quitting program.')
+    elif option == "q":
+        print(Fore.RED + "Quitting program.")
         break
     else:
-        print(Fore.LIGHTRED_EX + 'Invalid option selected. Try again.')
+        print(Fore.LIGHTRED_EX + "Invalid option selected. Try again.")
